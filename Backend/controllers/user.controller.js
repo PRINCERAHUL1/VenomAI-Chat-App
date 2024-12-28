6b4b16d73aba1a3f32b5e745f9639a3827b45e33
PRINCERAHUL1 <rmrahul258@gmail.com>
2024-12-28 17:03:10 +0530

    User Login & authUserMiddleware

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -18,3 +18,38 @@ export const CreateUserController = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+export const loginController = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email }).select('+password');
+
+        if(!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isMatch = await user.isValidPassword(password);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const token = user.generateToken();
+
+        res.status(200).json({ user, token });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+export const profileController = async (req, res) => {
+        console.log(req.user);
+
+        res.status(200).json({ user: req.user });
+};
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,15 +1,22 @@
 import {Router} from 'express';
 import * as userController from '../controllers/user.controller.js';
 import {body} from 'express-validator';
+import * as authMiddleware from '../middleware/auth.middleware.js';
 
 
 const router = Router();
 
 router.post('/register',
     body('email').isEmail().withMessage('Email must be a valid email'),
-    body('password').isLength({ min: 4}).withMessage('Password must be at least 6 characters'),
-
+    body('password').isLength({ min: 4}).withMessage('Password must be at least 4 characters'),
     userController.CreateUserController);
+
+router.post('/login',
+    body('email').isEmail().withMessage('Email must be a valid email'),
+    body('password').isLength({ min: 4}).withMessage('Password must be at least 4 characters'),
+    userController.loginController);
+
+router.get('/profile', authMiddleware.authUser, userController.profileController);
 
 
 export default router;
