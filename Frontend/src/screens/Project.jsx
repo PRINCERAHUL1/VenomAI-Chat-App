92790ce6a5ac44eb318488a7f923a734121fa015
PRINCERAHUL1 <rmrahul258@gmail.com>
2025-03-04 22:31:03 +0530

    Added Project UI

--- Frontend/src/screens/Project.jsx
+++ Frontend/src/screens/Project.jsx
@@ -9,7 +9,21 @@ const Project = () => {
   
     return (
     <div>
-        Project
+        <main
+        className='h-screen w-screen flex'
+        >
+
+            <section className="left h-full min-w-60 bg-red-300">
+
+                <header
+                className='flex justify-end p-2 px-4 w-full bg-slate-200'>
+                    <button className='p-2'>
+                        <i className="ri-group-fill"></i>
+                    </button>                    
+                </header>
+            </section>
+
+        </main>
     </div>
   )
 }
