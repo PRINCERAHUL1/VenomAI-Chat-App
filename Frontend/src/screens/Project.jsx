90508fb5c3781d4108c2d57899ead4c9b3a7df6b
PRINCERAHUL1 <rmrahul258@gmail.com>
2025-03-04 22:59:49 +0530

    Added Messaging UI

--- Frontend/src/screens/Project.jsx
+++ Frontend/src/screens/Project.jsx
@@ -13,14 +13,39 @@ const Project = () => {
         className='h-screen w-screen flex'
         >
 
-            <section className="left h-full min-w-60 bg-red-300">
+            <section className="left flex flex-col h-full min-w-72 bg-slate-300">
 
                 <header
-                className='flex justify-end p-2 px-4 w-full bg-slate-200'>
-                    <button className='p-2'>
+                className='flex justify-end p-2 px-4 w-full bg-slate-100'>
+                    <button className='p-2 px-4'>
                         <i className="ri-group-fill"></i>
                     </button>                    
                 </header>
+
+                <div className="conversation-area flex-grow flex flex-col">
+                    <div className="message-box p-1 flex-grow flex flex-col gap-1">
+                        <div className="message max-w-56 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
+                            <small
+                            className='opacity-65 text-xs'
+                            >example@gmail.com</small>
+                            <p className='text-sm'>lorem ipsum dolor sit amet. lore</p>
+                        </div>
+                        <div className="ml-auto message max-w-56 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
+                            <small
+                            className='opacity-65 text-xs'
+                            >example@gmail.com</small>
+                            <p className='text-sm'>lorem ipsum dolor sit amet</p>
+                        </div>
+                    </div>
+                        <div className="inputField w-full flex">
+                            <input 
+                            className='p-2 px-4 border-none outline-none'
+                            type="text" placeholder='Enter message'/>
+                            <button 
+                            className='flex-grow px-3'
+                            ><i className="ri-send-plane-fill"></i></button>
+                        </div>
+                </div>
             </section>
 
         </main>
