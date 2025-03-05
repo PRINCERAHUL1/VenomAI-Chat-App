de8efae112f2e3abf1fd563cfce25fde7889f660
PRINCERAHUL1 <rmrahul258@gmail.com>
2025-03-05 11:26:42 +0530

    Socket IO Setup Done

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 import http from 'http';
 import app from './app.js';
+import {Server} from 'socket.io';
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+
+const io = new Server(server);
+
+io.on('connection', socket => {
+    console.log('a user connected');
+    
+    socket.on('event', data => { /* … */ });
+    socket.on('disconnect', () => { /* … */ });
+});
 
 
 server.listen(PORT, () => {
