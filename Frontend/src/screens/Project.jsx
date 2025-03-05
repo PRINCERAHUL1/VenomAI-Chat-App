0efc5a70401a02d460ca6dcb7f2f37bd0c286473
PRINCERAHUL1 <rmrahul258@gmail.com>
2025-03-05 23:51:13 +0530

    Improved Message Handling & Integrated with UI

--- Frontend/src/screens/Project.jsx
+++ Frontend/src/screens/Project.jsx
@@ -12,6 +12,7 @@ const Project = () => {
     const [project, setProject] = useState(location.state.project)
     const [message, setMessage] = useState('')
     const {user} = useContext(UserContext)
+    const messageBox = React.createRef()
 
     const [users, setUsers] = useState([])
 
@@ -41,12 +42,13 @@ const Project = () => {
     }
 
     const send = () =>  {
-        console.log(user)
 
         sendMessage('project-message', {
             message,
-            sender: user._id
+            sender: user.email,
         })
+
+        appendOutgoingMessage(message);
 
         setMessage("")
     }
@@ -57,6 +59,7 @@ const Project = () => {
 
         receiveMessage('project-message', data => {
             console.log(data)
+            appendIncomingMessage(data);
         });
 
 
@@ -75,11 +78,42 @@ const Project = () => {
         })
     }, [])
 
+    function appendIncomingMessage(messageObject) {
+        const messageBox = document.querySelector('.message-box')
+
+        const message = document.createElement('div')
+        message.classList.add('message', 'max-w-56', 'flex', 'flex-col', 'p-2', 'bg-slate-50', 'w-fit', 'rounded-md')
+        message.innerHTML = `
+            <small class='opacity-65 text-xs'>${messageObject.sender}</small>
+            <p class='text-sm'>${messageObject.message}</p>
+        `
+        messageBox.appendChild(message)
+        scrollToBottom()
+    }
+
+    function appendOutgoingMessage(message){
+        const messageBox = document.querySelector('.message-box')
+
+        const messageElement = document.createElement('div')
+        messageElement.classList.add('ml-auto', 'message', 'max-w-56', 'flex', 'flex-col', 'p-2', 'bg-slate-50', 'w-fit', 'rounded-md')
+        messageElement.innerHTML = `
+            <small class='opacity-65 text-xs'>${user.email}</small>
+            <p class='text-sm'>${message}</p>
+        `
+        messageBox.appendChild(messageElement)
+        scrollToBottom()
+    }
+
+    function scrollToBottom(){
+        messageBox.current.scrollTop = messageBox.current.scrollHeight
+    }
+
+
     return (
         <div>
             <main className='h-screen w-screen flex'>
-                <section className="left relative flex flex-col h-full min-w-80 bg-slate-300">
-                    <header className='flex justify-between items-center p-2 px-4 w-full bg-slate-100'>
+                <section className="left relative flex flex-col h-screen min-w-80 bg-slate-300">
+                    <header className='flex justify-between items-center p-2 px-4 w-full bg-slate-100 absolute top-0'>
                         <button className='flex gap-2 ' onClick={() => setIsModalOpen(true)}>
                             <i className="ri-add-fill mr-1"></i>
                             <p>Add Collaborator</p>
@@ -89,18 +123,12 @@ const Project = () => {
                         </button>
                     </header>
 
-                    <div className="conversation-area flex-grow flex flex-col">
-                        <div className="message-box p-1 flex-grow flex flex-col gap-1">
-                            <div className="message max-w-56 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
-                                <small className='opacity-65 text-xs'>example@gmail.com</small>
-                                <p className='text-sm'>lorem ipsum dolor sit amet. lore</p>
+                    <div className="conversation-area pt-14 pb-10 flex-grow flex flex-col h-full relative">
+                            <div 
+                            ref={messageBox}
+                            className="message-box p-1 flex-grow flex flex-col gap-1 overflow-auto max-h-full scrollbar-hide">
                             </div>
-                            <div className="ml-auto message max-w-56 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
-                                <small className='opacity-65 text-xs'>example@gmail.com</small>
-                                <p className='text-sm'>lorem ipsum dolor sit amet</p>
-                            </div>
-                        </div>
-                        <div className="inputField w-full flex">
+                        <div className="inputField w-full flex absolute bottom-0">
                             <input 
                             value={message}
                             onChange={(e) => setMessage(e.target.value)}
