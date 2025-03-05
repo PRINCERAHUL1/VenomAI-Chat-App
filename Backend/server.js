ee743f4f50bbf49739d27274fccd6cd98554aa40
PRINCERAHUL1 <rmrahul258@gmail.com>
2025-03-05 11:49:37 +0530

    Socket Auth Middleware Done

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ dotenv.config();
 import http from 'http';
 import app from './app.js';
 import {Server} from 'socket.io';
+import jwt from 'jsonwebtoken';
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,9 +11,31 @@ const server = http.createServer(app);
 
 const io = new Server(server);
 
+io.use((socket, next) => {
+    try {
+        const token = socket.handshake.auth?.token || socket.handshake.headers.authorization?.split(' ')[1];
+        
+        if (!token) {
+            return next(new Error('Authentication error'));
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(!decoded) {
+            return next(new Error('Authentication error'));
+        }
+
+        socket.user = decoded;
+        next();
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+})
+
 io.on('connection', socket => {
     console.log('a user connected');
-    
+
     socket.on('event', data => { /* … */ });
     socket.on('disconnect', () => { /* … */ });
 });
